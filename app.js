850c7fb6543ed60b30436586d865a2eea5df0e5a
GREATBIGNATE <greatbignate@gmail.com>
2018-08-21 10:36:32 -0700

    it still doesn't work but it's cleaner now

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var allItems = [];
-var allItemTitles = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
+var allItemTitles = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','tauntaun','unicorn','usb','water-can','wine-glass'];
 var numberImageDisplay = 3;
 var picsUlEl = document.getElementById('itempics');
 
@@ -24,28 +24,54 @@ function presentItems() {
   var rando = Math.floor(allItems.length*Math.random());
   var compareArray = [];
   var safety = 0;
-  for (var i = 0; i<(numberImageDisplay-1); i++) {
-    if (i === 0) {
-      var imageEl = document.createElement('img');
-      compareArray.push(allItems[rando]);
-      imageEl.src = allItems[rando].path;
-      picsUlEl.appendChild(imageEl);
+  for (var n=0; n<(numberImageDisplay); n++) {
+    if (n===0){
+      compareArray.push(rando);
+      rando = Math.floor(allItems.length*Math.random());
+    } else if (compareArray[(n-1)] === rando && safety<100) {
+      n--;
+      safety++;
       rando = Math.floor(allItems.length*Math.random());
     } else {
-      for (var n = 0; n<(numberImageDisplay-1); n++){
-        if (allItems[rando] === compareArray[n] && safety <100) {
-          n--;
-          safety++;
-          rando = Math.floor(allItems.length*Math.random());
-        } else {
-          imageEl.src = allItems[rando].path;
-          picsUlEl.appendChild(imageEl);
-          compareArray.push(allItems[rando]);
-          rando = Math.floor(allItems.length*Math.random());
-        }
-      }
+      compareArray.push(rando);
+      safety++;
+      rando = Math.floor(allItems.length*Math.random());
     }
   }
+  for (var i=0; i<(numberImageDisplay); i++) {
+    console.log(compareArray[i]+ ' : ' +allItems[compareArray[i]].name);
+    if (i === 0) {
+      var imageEl = document.createElement('img');
+      imageEl.src = allItems[compareArray[i]].path;
+      picsUlEl.appendChild(imageEl);
+    } else {
+      imageEl = document.createElement('img');
+      imageEl.src = allItems[compareArray[i]].path;
+      picsUlEl.appendChild(imageEl);
+    }
+  }
+  // for (var i = 0; i<(numberImageDisplay-1); i++) {
+  //   if (i === 0) {
+  //     var imageEl = document.createElement('img');
+  //     compareArray.push(allItems[rando]);
+  //     imageEl.src = allItems[rando].path;
+  //     picsUlEl.appendChild(imageEl);
+  //     rando = Math.floor(allItems.length*Math.random());
+  //   } else {
+  //     for (var n = 0; n<(numberImageDisplay-1); n++){
+  //       if (allItems[rando] === compareArray[n] && safety <100) {
+  //         n--;
+  //         safety++;
+  //         rando = Math.floor(allItems.length*Math.random());
+  //       } else {
+  //         imageEl.src = allItems[rando].path;
+  //         picsUlEl.appendChild(imageEl);
+  //         compareArray.push(allItems[rando]);
+  //         rando = Math.floor(allItems.length*Math.random());
+  //       }
+  //     }
+  //   }
+  // }
 }
 
 presentItems();
