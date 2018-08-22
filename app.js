6e12fbbbf0aa04c86f3da89151a8794884212a5c
GREATBIGNATE <greatbignate@gmail.com>
2018-08-21 18:49:25 -0700

    got it mostly working now

--- app.js
+++ app.js
@@ -3,7 +3,13 @@
 var allItems = [];
 var allItemTitles = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
 var numberImageDisplay = 3;
-var picsLocation = document.getElementById('itempics');
+var leftImage = document.getElementById('leftimage');
+var midImage = document.getElementById('midimage');
+var rightImage = document.getElementById('rightimage');
+var counterTable = document.getElementById('countertable');
+var iterations = 0;
+let maxIterations = 25;
+
 
 //Create constructor function for SKUs named "Item"
 function Item(name) {
@@ -21,14 +27,31 @@ allItemTitles.forEach(function(itemName) {
   new Item(itemName);
 });
 
+//Create table function to display click counts -- call later
+Item.prototype.fillTable = function (){
+  var tableRowElement = document.createElement('tr');
+  var tableCellElement = document.createElement('td');
+  tableCellElement.textContent = this.name;
+  tableRowElement.appendChild(tableCellElement);
+  tableCellElement.textContent = this.timesShown;
+  tableRowElement.appendChild(tableCellElement);
+  tableCellElement.textContent = this.timesSelected;
+  tableRowElement.appendChild(tableCellElement);
+  tableCellElement.textContent = (this.timesSelected)/(this.timesShown);
+  tableRowElement.appendChild(tableCellElement);
+  tableCellElement.textContent = (this.timesSelected)/iterations;
+  tableRowElement.appendChild(tableCellElement);
+  counterTable.appendChild(tableRowElement);
+};
+
 function generateNonRepeatingArray() {
   var rando = Math.floor(allItems.length*Math.random());
   var compareArray = [];
   var safety = 0;
   for (var n=0; n<(numberImageDisplay); n++) {
-    if (numberImageDisplay>=allItems.length){
+    if (numberImageDisplay>=allItems.length/3){
       alert('Too many images to display for sample size');
-      break;
+      numberImageDisplay = 3;
     }else if (n===0){
       compareArray.push(rando);
       rando = Math.floor(allItems.length*Math.random());
@@ -44,17 +67,60 @@ function generateNonRepeatingArray() {
   }
   return compareArray;
 }
+
 var imageArray = generateNonRepeatingArray(); //Defines imageArray outside of a function to be referenced in multiple functions
 
-function presentItems(){
-  for (var i=0; i<(numberImageDisplay); i++) {
-    console.log(imageArray[i]+ ' : ' +allItems[imageArray[i]].name);
-    var newPic = document.createElement('img');
-    newPic.src = allItems[imageArray[i]].path;
-    document.body.insertBefore(newPic, picsLocation);
-    allItems[imageArray[i]].timesShown++;
-  }
+function renderImages(event) {
+  leftImage.src = allItems[imageArray[0]].path;
+  leftImage.title = allItems[imageArray[0]].name;
+  allItems[imageArray[0]].timesShown++;
+  midImage.src = allItems[imageArray[1]].path;
+  midImage.title = allItems[imageArray[1]].name;
+  allItems[imageArray[1]].timesShown++;
+  rightImage.src = allItems[imageArray[2]].path;
+  rightImage.title = allItems[imageArray[2]].name;
+  allItems[imageArray[2]].timesShown++;
 }
 
-presentItems();
+renderImages();
 
+if (iterations<maxIterations) {
+  leftImage.addEventListener('click', function(event) {
+    console.log(event.target);
+    console.log(iterations);
+    allItems[imageArray[0]].timesSelected++;
+    iterations++;
+    imageArray = generateNonRepeatingArray();
+    renderImages(event);
+  });
+
+  midImage.addEventListener('click', function(event) {
+    console.log(event.target);
+    console.log(iterations);
+    allItems[imageArray[1]].timesSelected++;
+    iterations++;
+    imageArray = generateNonRepeatingArray();
+    renderImages(event);
+  });
+
+  rightImage.addEventListener('click', function(event) {
+    console.log(event.target);
+    console.log(iterations);
+    allItems[imageArray[1]].timesSelected++;
+    iterations++;
+    imageArray = generateNonRepeatingArray();
+    renderImages(event);
+  });
+} else {
+  var headerArray = ['Item','# Times Presented','# Times Selected','Selection Rate','% of Selections'];
+  var tableRowElement = document.createElement('tr');
+  var tableHeaderElement = document.createElement('th');
+  for (var i=0; i<headerArray.length; i++) {
+    tableHeaderElement.textConent = headerArray[i];
+    tableRowElement.appendChild(tableHeaderElement);
+  }
+  counterTable.appendChild(tableRowElement);
+  for (var i=0; i<allItems; i++){
+    allItems[i].fillTable();
+  }
+}
